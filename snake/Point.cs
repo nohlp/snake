f68c98eaf3a34485e642dd024d3d1f3d8ff12fac
AzarovA <nonamehelp@gmail.com>
2018-02-14 11:23:00 +0300

    lesson 1 end

---
+++ snake/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace nohlp_snake
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point(){
+            
+        }
+
+        public Point(int _x,int _y,char _sym){
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw()
+        {
+
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+
+    }
+
+}
